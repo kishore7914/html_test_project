24802d8769b4d7423f4aa4ebdc8d9f0933716155
kishore914 <saikishorereddyguntaka@gmail.com>
2024-06-11 23:06:47 +0530

    update

--- number count/script.js
+++ number count/script.js
@@ -4,18 +4,52 @@ let curValue = 0;
 const btnDecrement = document.querySelector('#decrement');
 const btnReset = document.querySelector('#reset');
 const btnIncrement = document.querySelector('#increment');
+const evenodd= document.querySelector('#evenodd');
 
-btnIncrement.addEventListener('click', () => {
+btnIncrement.addEventListener('click', function inc() 
+ {
+
   curValue++;
-  mainTitle.textContent = curValue;z
+  if(curValue>20)
+    {
+        curValue=0;
+        alert("max 20 only allowed")
+    }
+    if(curValue%2==0){
+        document.getElementById('odd').hidden=true;
+        document.getElementById('even').hidden=false;
+    }
+    else{
+        document.getElementById('even').hidden=true;
+        document.getElementById('odd').hidden=false;
+    }
+   
+  mainTitle.textContent = curValue;
 });
+btnDecrement.addEventListener('click',function dec() 
+{
+    curValue--;
+  if(curValue<0)
+{
+    curValue=0;
+    alert("-ve not allowed")
+}
+if(curValue%2==0){
+    document.getElementById('odd').hidden=true;
+    document.getElementById('even').hidden=false;
+}
+else{
+    document.getElementById('even').hidden=true;
+    document.getElementById('odd').hidden=false;
+}
 
-btnDecrement.addEventListener('click', () => {
-  curValue--;
   mainTitle.textContent = curValue;
 });
 
-btnReset.addEventListener('click', () => {
+btnReset.addEventListener('click',  function reset() 
+ {
   curValue = 0;
   mainTitle.textContent = curValue;
+  document.getElementById('even').hidden=false;
+  document.getElementById('odd').hidden=true;
 });
